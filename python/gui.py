377beabd28ddcbc04e5caaa3de6bbc63a9a86eaf
soapchan <noahfoxgaming@gmail.com>
2024-04-04 17:01:53 +1100

    Setup gui.py

---
+++ python/gui.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+
+class Mainpage:
+	def __init__(self):
+		self.mainpage = tk.Tk()
+
+		self.item_label = tk.Label(text="Item: ")
+		self.item_entry = tk.Entry()
+
+		self.model_label = tk.Label(text="Model: ")
+		self.model_entry = tk.Entry()
+
+		self.run_button = tk.Button(text="Run")
+
+
+	def grid_features(self):
+		self.item_label.grid(column=1, row=1)
+		self.item_entry.grid(column=2, row=1)
+
+		self.model_label.grid(column=1, row=2)
+		self.model_entry.grid(column=2, row=2)
+
+		self.run_button.grid(column=1, row=3)
